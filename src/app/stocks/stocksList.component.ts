e65ac24b5f4143ae4be51ba9990dd5e25cdac093
Mike Roy <michael.roy@amadeus.com>
2018-02-06 16:44:01 -0500

    fixed observable array of stocks

--- src/app/stocks/stocksList.component.ts
+++ src/app/stocks/stocksList.component.ts
@@ -2,7 +2,9 @@ import {Component, OnInit} from '@angular/core';
 import {Stock} from './stock';
 import {StockService} from './stock.service';
 import {Observable} from 'rxjs/Observable';
-import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/do';
 
 @Component({
   selector: 'app-stocks',
@@ -11,7 +13,7 @@ import {HttpClient, HttpClientModule} from '@angular/common/http';
 })
 
 export class StocksListComponent implements OnInit {
-  constructor(private http: HttpClient) {
+  constructor(private $http: HttpClient) {
   }
   stockUrl = 'http://localhost:8000/api/stocks';
   title = 'Stock Matrix';
@@ -21,7 +23,8 @@ export class StocksListComponent implements OnInit {
   colHead_StockPrice = 'Price';
   colHead_Change = 'Change';
   colHead_ChangePercent = 'Change Percent';
-  stockList: Stock[];
+  stockList: Stock[] = [];
+  tempStock: any;
   /*
   ngOnInit(): void {
     this.stockList = this.stockService.getStocks();
@@ -29,9 +32,23 @@ export class StocksListComponent implements OnInit {
     console.log('stock list' + this.stockList);
   }
   */
-  ngOnInit(): void {
-    this.http.get('http://localhost:8000/api/stocks')
+  getStocks(): Observable<Stock[]> {
+    return this.$http.get<Stock[]>('http://localhost:8000/api/stocks');
+ //     .do(data => console.log('All: ' + JSON.stringify(data)))
+ //     .catch(this.handleError);
+  }
+  printStocks(): void {
+    this.$http.get('http://localhost:8000/api/stocks')
       .subscribe(response => console.log(response));
   }
+  testPrint(tempStock) {
+    console.log(tempStock);
+}
+  ngOnInit(): void {
+    this.getStocks().subscribe(stocks => this.stockList = stocks);
+    this.printStocks();
+  }
+
+
 }
 
