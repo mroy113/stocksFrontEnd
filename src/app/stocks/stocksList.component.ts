9219a62b485e014815c15b4419c1bcf3d3f28d3b
Mike Roy <michael.roy@amadeus.com>
2018-02-12 19:05:23 -0500

    implemented live updates; created bug where stockList does not stay sorted upon updating.

--- src/app/stocks/stocksList.component.ts
+++ src/app/stocks/stocksList.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Stock} from './stock';
 import {StockService} from './stock.service';
+import {Observable} from 'rxjs/Rx';
 
 @Component({
   selector: 'app-stocks',
@@ -9,35 +10,68 @@ import {StockService} from './stock.service';
 
 export class StocksListComponent implements OnInit {
   constructor(public $stockService: StockService) {
+    this.subscribeToStocks();
   }
   title = '<(o_O<)  Stock Matrix  (>O_o)>';
-  columnHeaders: String[] = [
-    ' ',
-    'Company',
-    'Price',
-    'Change',
-    'Change Percent'
-  ];
   stockList: Stock[];
-  sortedStockList: Stock[];
+  companySortSwitch = false;
+  priceSortSwitch = false;
 
-  sortList(): void {
-    console.log('clicked filter Button');
-    this.sortedStockList = this.stockList;
-    this.sortStockList();
+  private subscribeToStocks() {
+    Observable.timer(0, 5000).subscribe(() => {
+      this.getAllStocks();
+    });
   }
-  sortStockList(): void {
-    this.sortedStockList.sort((stockA, stockB) => {
-      const nameA = stockA.companyName.toLocaleLowerCase();
-      const nameB = stockB.companyName.toLocaleLowerCase();
-      if (nameA > nameB) { return 1; }
-      if (nameA < nameB) { return -1; }
-      return 0;
+  private getAllStocks() {
+    this.$stockService.getStocks().subscribe(stocks => this.stockList = stocks);
+    console.log('getAllStocks Called');
+  }
+  public toggleSortCompanyName(): void {
+      if (this.companySortSwitch === false) {
+        this.sortStockListAscending('companyName');
+      } else {
+        this.sortStockListDescending('companyName');
+      }
+      this.companySortSwitch = !this.companySortSwitch;
+    }
+  public toggleSortPrice(): void {
+    if (this.priceSortSwitch === false) {
+      this.sortStockListAscending('stockPrice_current');
+    } else {
+      this.sortStockListDescending('stockPrice_current');
+    }
+    this.priceSortSwitch = !this.priceSortSwitch;
+  }
+  private sortStockListAscending(atribToSort: string): void {
+    this.stockList.sort((stockA, stockB) => {
+        if (atribToSort === 'companyName') {
+          if (stockA[atribToSort] > stockB[atribToSort]) {
+            return 1;
+          }
+          if (stockA[atribToSort] < stockB[atribToSort]) {
+            return -1;
+          }
+          return 0;
+        } else {
+          return stockB[atribToSort] - stockA[atribToSort];
+        }
+    });
+  }
+  private sortStockListDescending(atribToSort: string): void {
+    this.stockList.sort((stockA, stockB) => {
+      if (atribToSort === 'companyName') {
+        if (stockA[atribToSort] < stockB[atribToSort]) {
+          return 1;
+        }
+        if (stockA[atribToSort] > stockB[atribToSort]) {
+          return -1;
+        }
+        return 0;
+      } else {
+        return stockA[atribToSort] - stockB[atribToSort];
+      }
     });
   }
   ngOnInit(): void {
-    this.$stockService.getStocks().subscribe(stocks => this.stockList = stocks);
-
   }
 }
-
